225bcd7cafba84886a5faa703b8087a1de356aee
PJ-clipper <pejavrajich@gmail.com>
2025-04-24 18:26:31 -0600

    fix: handle moviepy uninstall automatically

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python -m pip install --upgrade pip
 pip install -r Backend/requirements.txt
-pip uninstall moviepy
+yes | pip uninstall moviepy
 pip install moviepy==1.0.3
 pip install imageio==2.31.1
 pip install imageio-ffmpeg==0.4.9 
